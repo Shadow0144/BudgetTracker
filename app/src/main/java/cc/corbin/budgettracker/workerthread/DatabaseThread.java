f210bf6c561cf500b53addac06540d4fcc0f2bd8
Shadow0144 <CorbinCogswell@gmail.com>
2018-06-09 20:57:51 +0900

    Fixed bug with budgets not resorting correctly

--- app/src/main/java/cc/corbin/budgettracker/workerthread/DatabaseThread.java
+++ app/src/main/java/cc/corbin/budgettracker/workerthread/DatabaseThread.java
@@ -303,7 +303,7 @@ public class DatabaseThread extends Thread
                 String[] newCategoryNames = event.getNewCategoryNames();
                 for (int i = 0; i < newCategoryNames.length; i++)
                 {
-                    _dbE.expenditureDao().updateCategoryNumber(newCategoryNames[i], i);
+                    _dbB.budgetDao().updateCategoryNumber(newCategoryNames[i], i);
                 }
                 break;
         }
