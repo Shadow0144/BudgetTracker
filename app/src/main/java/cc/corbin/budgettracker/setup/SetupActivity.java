c68a343548fd137bda1e3f128e107a8c80c2aedb
Shadow0144 <CorbinCogswell@gmail.com>
2019-09-09 21:53:25 +0900

    Setting up first-time setup activity

---
+++ app/src/main/java/cc/corbin/budgettracker/setup/SetupActivity.java
@@ -0,0 +1,16 @@
+package cc.corbin.budgettracker.setup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cc.corbin.budgettracker.R;
+
+public class SetupActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
---
+++ app/src/main/java/cc/corbin/budgettracker/setup/SetupBaseCurrencyFragment.java
@@ -0,0 +1,7 @@
+package cc.corbin.budgettracker.setup;
+
+import android.support.v4.app.Fragment;
+
+public class SetupBaseCurrencyFragment extends Fragment
+{
+}
